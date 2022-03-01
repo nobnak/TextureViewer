f4229ac742e2d61b13e33437b7a9cdc98bccf217
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-03-02 02:34:41 +0900

    UI 更新

--- Scripts/PIPTextureWindow.cs
+++ Scripts/PIPTextureWindow.cs
@@ -1,5 +1,6 @@
 using Gist2.Extensions.ScreenExt;
 using Gist2.Extensions.SizeExt;
+using Gist2.GUIExt;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +14,11 @@ namespace PIP {
 
         protected Rect windowSize = new Rect(10, 10, 10, 10);
         protected PIPTextureMaterial mat;
+
+        protected bool compactList;
+        protected bool visibleOptions;
+        protected int selectedTexIndex;
+        protected bool fullscreen;
 
         #region unity
         private void OnEnable() {
@@ -29,6 +35,12 @@ namespace PIP {
             }
         }
         private void OnGUI() {
+            var screenSize = ScreenExtension.ScreenSize();
+            if (fullscreen && selectedTexIndex >= 0 && selectedTexIndex < textures.Count) {
+                var tex = textures[selectedTexIndex];
+                tex.DrawTexture(screenSize.x, screenSize.y, mat);
+            }
+
             windowSize.size = Vector2Int.zero;
             if (tuner.windowIsOpen) windowSize = GUILayout.Window(GetInstanceID(), windowSize, Window, name);
         }
@@ -42,18 +54,43 @@ namespace PIP {
             var texGap = tuner.texGap;
             var texOffset = (float)texGap;
 
-            using (new GUILayout.HorizontalScope()) {
-                foreach (var tex in textures) {
-                    if (tex.isActiveAndEnabled && tex.Value != null) {
-                        using (new GUILayout.VerticalScope()) {
-                            var texSize = tex.Value.Size();
-                            var texAspect = texSize.Aspect();
-                            var texWidth = texAspect * texHeight;
+            using (new GUILayout.VerticalScope()) {
 
-                            GUILayout.Label(tex.gameObject.name);
-                            tex.DrawTexture(texWidth, texHeight, mat);
+                using (new GUILayout.HorizontalScope()) {
+                    for (var i = 0; i < textures.Count; i++) {
+                        var tex = textures[i];
+
+                        if (compactList && i != selectedTexIndex) continue; 
+
+                        if (tex != null && tex.isActiveAndEnabled && tex.Value != null) {
+                            using (new GUILayout.VerticalScope()) {
+                                var texSize = tex.Value.Size();
+                                var texAspect = texSize.Aspect();
+                                var texWidth = texAspect * texHeight;
+
+                                GUILayout.Label(tex.gameObject.name);
+                                tex.DrawTexture(texWidth, texHeight, mat);
+                            }
+                            GUILayout.Space(texGap);
                         }
-                        GUILayout.Space(texGap);
+                    }
+                }
+
+                using (new GUILayout.HorizontalScope()) {
+                    if (GUILayout.Button("<")) selectedTexIndex--;
+                    if (GUILayout.Button(">")) selectedTexIndex++;
+                    GUILayout.Label($"{selectedTexIndex}");
+                    GUILayout.FlexibleSpace();
+
+                    var totalTex = textures.Count;
+                    selectedTexIndex = (totalTex == 0) ? -1 : (selectedTexIndex + totalTex) % totalTex;
+                }
+
+                visibleOptions = visibleOptions.FoldOut("Options");
+                if (visibleOptions) {
+                    using (new GUILayout.VerticalScope()) {
+                        compactList = GUILayout.Toggle(compactList, "Compact");
+                        fullscreen = GUILayout.Toggle(fullscreen, "fullscreen");
                     }
                 }
             }
