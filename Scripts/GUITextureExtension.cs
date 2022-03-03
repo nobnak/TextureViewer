693052ddda0db4b0acd6fb98cf1d68300d5b73f6
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-03-03 12:18:02 +0900

    処理の共通部分メソッドへ

--- Scripts/GUITextureExtension.cs
+++ Scripts/GUITextureExtension.cs
@@ -7,17 +7,20 @@ namespace PIP {
 
     public static class GUITextureExtension {
 
+        public static Texture DrawTexture(this Texture tex, Rect rect, Material mat = null) {
+            if (Event.current.type == EventType.Repaint) Graphics.DrawTexture(rect, tex, mat);
+            return tex;
+        }
+
         public static Rect DrawTexture(this Texture tex, float texWidth, float texHeight, Material mat = null) {
             var rect = GUILayoutUtility.GetRect(texWidth, texHeight);
-            if (Event.current.type == EventType.Repaint) Graphics.DrawTexture(rect, tex, mat);
+            tex.DrawTexture(rect, mat);
             return rect;
         }
         public static Rect DrawTexture(this PIPTextureHolder tex, float texWidth, float texHeight, PIPTextureMaterial mat = null) {
             var rect = GUILayoutUtility.GetRect(texWidth, texHeight);
-            if (Event.current.type == EventType.Repaint) {
-                if (mat != null) tex.SetData(mat);
-                Graphics.DrawTexture(rect, tex, mat);
-            }
+            if (mat != null) tex.SetData(mat);
+            tex.Value.DrawTexture(rect, mat);
             return rect;
         }
 
